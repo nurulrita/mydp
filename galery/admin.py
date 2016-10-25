ae467414a54c815f3edcf1766b53d7e1afe4286a
nuthanu <nuthanuu@gmail.com>
2016-10-25 15:18:34 +0700

    gara-gara namespace jadi banyak error :p

--- galery/admin.py
+++ galery/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Galery
+from .models import Galery, Category
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    
 admin.site.register(Galery)
+admin.site.register(Category, CategoryAdmin)
--- galery/urls.py
+++ galery/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
     url(r'^$', views.galery, name='galery'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+	url(r'^photo/(?P<category_name_slug>[\w\-]+)/(?P<photo_slug>[\w\-]+)/$', views.photo, name='photo'),
 ]
