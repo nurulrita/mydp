399e978684ced2aa93878af65179f280bfb9d507
nuthanu <nuthanuu@gmail.com>
2016-10-26 11:36:48 +0700

    add image in choice

--- polls/models.py
+++ polls/models.py
@@ -29,6 +29,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    image = models.ImageField()
     
     def __str__(self):
         return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from .models import Question, Choice
 from django.views import generic
 from django.utils import timezone
 
