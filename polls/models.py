5e8d9091017f1d474f1c6e7631722707cc645896
nuthanu <nuthanuu@gmail.com>
2016-11-01 09:42:11 +0700

    almost finish

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils import timezone
 
+
 @python_2_unicode_compatible
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
