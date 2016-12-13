d02dd3dd5a89a97040223c6a21e5b3187849cd78
nuthanu <nuthanuu@gmail.com>
2016-12-13 21:34:04 +0700

    delete comment and several html files in blog

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Post, Comment 
+from .models import Post 
 
 class PostForm(forms.ModelForm):
 
@@ -8,8 +8,3 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ('title', 'text',)
 
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ('author', 'text',)
